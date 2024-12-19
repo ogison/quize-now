2c7bdeecd308eadec952eb26e9f37f6f5ed4485b
ogison <meyo.ogs9@gmail.com>
2024-12-19 22:51:16 +0900

    Initialized: update settings

--- quize-front/postcss.config.mjs
+++ quize-front/postcss.config.mjs
@@ -2,6 +2,23 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    tabWidth: 4,
+    printWidth: 120,
+    overrides: [
+      {
+        files: "*.{json,json5,html,yaml,yml}",
+        options: {
+          tabWidth: 2,
+        },
+      },
+      {
+        files: "*.md",
+        options: {
+          tabWidth: 2,
+          trailingComma: "none",
+        },
+      },
+    ],
   },
 };
 
